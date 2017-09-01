933c6b016f8031cdf64fc43e8a12276d769e1211
Pete Corey <petecorey@gmail.com>
2017-09-01 14:56:00 -0400

    Fixed bug

--- lib/hello_bitcoin.ex
+++ lib/hello_bitcoin.ex
@@ -3,13 +3,14 @@ defmodule HelloBitcoin do
   def bitcoin_rpc(method, params \\ []) do
     with url <- Application.get_env(:hello_bitcoin, :bitcoin_url),
          command <- %{jsonrpc: "1.0", method: method, params: params},
-         body <- Poison.encode!(command),
+         {:ok, body} <- Poison.encode(command),
          {:ok, response} <- HTTPoison.post(url, body),
          {:ok, metadata} <- Poison.decode(response.body),
          %{"error" => nil, "result" => result} <- metadata do
       {:ok, result}
     else
       %{"error" => reason} -> {:error, reason}
+      error -> error
     end
   end
 
